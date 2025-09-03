db12aace3b79ea29243468874ce9500b42c93d66
Deniz Koc <dkocusa@outlook.com>
2025-09-03 13:17:37 -0500

    feat(tickets): list + emergency filter; move ticketService

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketService.js"
 
-
+import "./App.css"
 
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const [allTickets, setAllTickets] = useState([])
+  const [filteredTickets, setFilteredTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
 
-  const handleBtnClick = () => {
-    setCount(count+1)
-    console.log(count)
-  }
+ 
+  useEffect(() => {
+    getAllTickets().then(ticketArray => {
+      setAllTickets(ticketArray)
+      setFilteredTickets(ticketArray) 
+    })
+  }, [])
+
+  
+  useEffect(() => {
+    if (showEmergencyOnly) {
+      const emergency = allTickets.filter(ticket => ticket.emergency === true)
+      setFilteredTickets(emergency)
+    } else {
+      setFilteredTickets(allTickets)
+    }
+  }, [showEmergencyOnly, allTickets]) 
+
   return (
-   <>
-      <h1>Hello!</h1>
-      <div>This is Amazing!</div>
-      <button className="btn-secondary" onClick={handleBtnClick}>Click me!</button>
-      <div>count: {count}</div>
-   </>
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <div>
+        <button className="filter-btn btn-primary" onClick={() => setShowEmergencyOnly(true)}>Emergency</button>
+        <button className="filter-btn btn-info" onClick={() => setShowEmergencyOnly(false)}>Show All</button>
+      </div>
+      <article className="tickets">
+        {filteredTickets.map(ticket => (
+          <section className="ticket" key={ticket.id}>
+            <header className="ticket-info">#{ticket.id}</header>
+            <div>{ticket.description}</div>
+            <footer>
+              <div className="ticket-info">emergency</div>
+              <div>{ticket.emergency ? "yes" : "no"}</div>
+            </footer>
+          </section>
+        ))}
+      </article>
+    </div>
   )
 }
